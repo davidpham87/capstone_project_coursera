365ecb8e74c4dccc5e41408d63c0b0b92fde14d8
David Pham <davidpham87@gmail.com>
2014-10-12 16:16:51 +0200

    Fixing bugs

--- shiny_apps/ui.R
+++ shiny_apps/ui.R
@@ -5,9 +5,9 @@ shinyUI(fluidPage(
     p('This is the shiny project for text prediction'),
     p('It is not really hard to see how to use the app: Write a simple sentences in the text box below and then after a few seconds the app will give you a list of 20 words (ordered by the most probable) which might complete the input!'),
     p('The sources files can be found at'),
-    a('Github Repo', 'https://github.com/davidpham87/capstone_project_coursera'),
-    p("The two *.rmd files explaining the Data processing and the Modelling part are published on the same website"),
-    a("Reproducible report", href = "http://rpubs.com/davidpham87/19545")  
+    a('Github Repo', href='https://github.com/davidpham87/capstone_project_coursera'),
+    p("The pdf file explaining the Data processing and the Modelling part are published on the same website"),
+    a("Reproducible report", href = "https://github.com/davidpham87/capstone_project_coursera/blob/master/report/word_prediction_report_DavidPham.pdf")  
     ),
   
                                         # Copy the line below to make a slider bar
@@ -29,4 +29,6 @@ shinyUI(fluidPage(
       )
     )
   )
-)        
+)       
+
+# shinyapps::configureApp("coursera_capstone_data_science_project", size="xlarge")
